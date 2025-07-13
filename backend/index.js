0bec07476ddfbb838d982543b4327ab254159b4a
vanshika2818 <saxenavanshika18mar@gmail.com>
2025-07-13 16:55:29 +0530

    🛠️ updated backend logic / connected DB

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js"
+import productRoutes from "./routes/productRoutes.js"
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 
-connectDB(); // 🚀 This connects MongoDB Atlas
+connectDB(); 
+app.get("/", (req, res) => {
+  res.send("backend is running");
+});
+
+ app.use("/api/auth", authRoutes);
+ app.use("/api/products", productRoutes); // 🚀 This connects MongoDB Atlas
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
